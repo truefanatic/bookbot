913bd3c5e743b4561ffc21f899d1ed1b41eeae83
Kyrylo Garmash <kirillgarmash@gmail.com>
2025-07-02 21:45:17 -0600

    final

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count, sort_dict
+
 
 def main():
-    num_words = get_num_words("books/frankenstein.txt")
-    print (f"{num_words} words found in the document")
+    book_path = "books/frankenstein.txt"
+    num_words = get_num_words(book_path)
+    dict = get_char_count(book_path)
+    sorted_dict = sort_dict(dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for d in sorted_dict:
+        val = list(d.values())
+        print(val[0] + ": " + str(val[1]))
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,31 @@
-def get_num_words(path_to_file):
+def get_file_content(path_to_file):
     with open(path_to_file) as f:
-        num_words = len( f.read().split() )
-    return num_words
+        return f.read()
+
+
+def get_num_words(path_to_file):
+    return len(get_file_content(path_to_file).split())
+
+
+def get_char_count(path_to_file):
+    content = get_file_content(path_to_file).lower()
+    dictionary = {}
+    for char in content:
+        if char in dictionary:
+            dictionary[char] += 1
+        else:
+            dictionary[char] = 1
+    return dictionary
+
+
+def sort_dict(dict): 
+    list_dict = []
+
+    def sort_on(items):
+        return items["num"]
+    
+    for key in dict:
+        if key.isalpha():
+            list_dict.append({"char": key, "num": dict[key]})
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
