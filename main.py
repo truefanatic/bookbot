c118c17d462c0891edcd0090bfae473f2abe9409
Kyrylo Garmash <kirillgarmash@gmail.com>
2025-07-02 22:07:00 -0600

    very final

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import get_num_words, get_char_count, sort_dict
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv)!= 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     num_words = get_num_words(book_path)
     dict = get_char_count(book_path)
     sorted_dict = sort_dict(dict)
